1c9795829887fc6f0e70b3d7f015db702ccfbf45
johnoneil <oneil.john@gmail.com>
2013-11-24 20:28:30 -0800

    Added test files.

--- test/main.cpp
+++ test/main.cpp
@@ -11,9 +11,44 @@ typedef int Int32;
 #include <iostream>
 #include <vector>
 
+#
+
 int main(int argc, char* argv[])
 {
-	IntBuffer::FreeGamesData freeGamesEvent;
+	//Imagine we have some game data
+	const Int32 win=3000;
+	const Int32 numFreeSpins=10;
+	
+	IntBuffer::FreeGamesData data;
+	data.SetWin(win);
+	IntBuffer::FreeGames& freegames = data.GetFreeGames();
+	freegames.SetWin(win);
+	for(Int32 spin=0;spin<numFreeSpins;++spin)
+	{
+		IntBuffer::Spin spin;
+		const Int32 reelCount=spin.StopCount();
+		for(Int32 stop=0;stop<reelCount;++stop)
+		{
+			spin.AddStop(stop);
+			spin.SetPrize(300);
+		}
+		freegames.AddSpin(spin);
+	}
+
+	//Dump current class data to screen
+
+	//write class data to integer buffer
+	const Int32 size=data.Size();
+	std::vector<Int32> buffer;
+	buffer.reserve(size);
+	data.Write(buffer);
+
+	//Read buffer data into another instance of class
+	IntBuffer::FreeGamesData readData=IntBuffer::FreeGamesData::Parse(buffer);
+
+	//Dump contents of class to screen.
+
+
 	return 0;
 }
 
